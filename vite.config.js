89caf3d9da3054e5937086ac49b0095f19f5321b
rayhan108 <raihanshorker56@gmail.com>
2026-02-07 14:48:40 +0600

    add ci/cd

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dashboard/", 
   plugins: [react()],
+  base: "/dashboard/", 
   server: {
     host: '0.0.0.0',  
   },
