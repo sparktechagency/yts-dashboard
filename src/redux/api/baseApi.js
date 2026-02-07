ff415983ec1abd6eb4bd98ae8774a7ad634492fc
rayhan108 <raihanshorker56@gmail.com>
2026-02-07 13:16:56 +0600

    add ci/cd

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -4,7 +4,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 const baseQuery = fetchBaseQuery({
     baseUrl: "https://backend.yourtradesource.com/api/v1",
     // baseUrl: "http://10.10.20.13:5000/api/v1",
-    // credentials: "include",c
+    // credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const token = (getState()).auth.token;
       if (token) {
